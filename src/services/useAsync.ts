40a9efe1d56102f464f44ba2170c83fae607bf15
kuum97 <kuum97@icloud.com>
2024-05-03 14:59:35 +0900

    Repactor useAsync for type

--- src/services/useAsync.ts
+++ src/services/useAsync.ts
@@ -1,24 +1,37 @@
 import { useCallback, useEffect, useState } from "react";
+interface CustomAsyncReturns<T> {
+  isLoading: boolean;
+  value: T | null;
+  error: Error | null;
+}
 
-const useAsync = (asyncFunc, userId, folderId) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [value, setValue] = useState({});
-  const [error, setError] = useState(null);
+type asyncFunc = <T>(userId?: number, folderId?: number) => Promise<T>;
+
+const useAsync = <T>(
+  asyncFunc: asyncFunc,
+  userId: number,
+  folderId: number
+): CustomAsyncReturns<T> => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [value, setValue] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
 
   const fetchData = useCallback(async () => {
     setIsLoading(true);
     try {
       let result;
       if (userId && folderId) {
-        result = await asyncFunc(userId, folderId);
+        result = await asyncFunc<T>(userId, folderId);
       } else if (userId) {
-        result = await asyncFunc(userId);
+        result = await asyncFunc<T>(userId);
       } else {
-        result = await asyncFunc();
+        result = await asyncFunc<T>();
       }
       setValue(result);
-    } catch (error) {
-      setError(error);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error);
+      }
     } finally {
       setIsLoading(false);
     }
