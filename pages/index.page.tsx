4b2e7a0eb57e10a48a89801761706e1102d1670d
kuum97 <kuum97@icloud.com>
2024-05-21 12:10:02 +0900

    Add link at home for move page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Link href="/signin">로그인</Link>
+      <Link href="/signup">회원가입</Link>
+      <Link href="/shared">공유페이지</Link>
+      <Link href="/folder">폴더페이지</Link>
+    </div>
+  );
 }
