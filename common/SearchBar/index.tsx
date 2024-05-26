2d85e6587fd7559b05247ae6bae821b57f056742
kuum97 <kuum97@icloud.com>
2024-05-26 18:38:28 +0900

    Add search query text for ux

--- common/SearchBar/index.tsx
+++ common/SearchBar/index.tsx
@@ -9,24 +9,32 @@ interface SearchValue {
 
 function SearchBar() {
   const { register, handleSubmit } = useForm<SearchValue>();
-  const { setSearchQuery } = useStoreState();
+  const { setSearchQuery, searchQuery } = useStoreState();
 
   const onSubmit = (data: SearchValue) => {
     setSearchQuery(data.searchInput);
   };
 
   return (
-    <form className={styles.wrapper} onSubmit={handleSubmit(onSubmit)}>
-      <button className={styles.button} type="submit">
-        <FaSearch />
-      </button>
-      <input
-        className={styles.input}
-        {...register("searchInput")}
-        type="text"
-        placeholder="링크를 검색해 보세요."
-      />
-    </form>
+    <>
+      <form className={styles.wrapper} onSubmit={handleSubmit(onSubmit)}>
+        <button className={styles.button} type="submit">
+          <FaSearch />
+        </button>
+        <input
+          className={styles.input}
+          {...register("searchInput")}
+          type="text"
+          placeholder="링크를 검색해 보세요."
+        />
+      </form>
+      {searchQuery && (
+        <p className={styles.queryMessage}>
+          <span className={styles.query}>{searchQuery}</span>으로 검색한
+          결과입니다.
+        </p>
+      )}
+    </>
   );
 }
 
