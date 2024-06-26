11122286b7e2ed37c97fafb8e68064954344e855
kuum97 <kuum97@icloud.com>
2024-06-26 13:24:54 +0900

    Feat: zustand 코드 정리 및 비밀번호 가시성 상태 추가

--- hooks/state/index.ts
+++ hooks/state/index.ts
@@ -4,25 +4,42 @@ import { create } from "zustand";
 interface StoreStateProps {
   user: UserData | null;
   setUser: (user: UserData) => void;
+
   folders: NewFolderData[] | FolderData[] | null;
   setFolders: (folders: NewFolderData[] | FolderData[]) => void;
+
   links?: LinkData[];
   setLinks: (links?: LinkData[]) => void;
+
   isLoadingWindow: boolean;
   setIsLoadingWindow: (isLoadingWindow: boolean) => void;
+
   searchQuery?: string;
   setSearchQuery: (searchQuery?: string) => void;
+
+  currentType: string;
+  toggleType: () => void;
 }
 
 export const useStoreState = create<StoreStateProps>((set) => ({
   user: null,
   setUser: (user: UserData) => set({ user }),
+
   folders: null,
   setFolders: (folders: NewFolderData[] | FolderData[]) => set({ folders }),
+
   links: [],
   setLinks: (links?: LinkData[]) => set({ links }),
+
   isLoadingWindow: true,
   setIsLoadingWindow: (isLoadingWindow: boolean) => set({ isLoadingWindow }),
+
   searchQuery: "",
   setSearchQuery: (searchQuery?: string) => set({ searchQuery }),
+
+  currentType: "password",
+  toggleType: () =>
+    set((state) => ({
+      currentType: state.currentType === "password" ? "text" : "password",
+    })),
 }));
